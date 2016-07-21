b14607c8fa6e1848a4ca1ae18dfec091b55c3386
leearaneta <lee.araneta@gmail.com>
2016-07-21 16:49:19 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  var img = $('img')[$('img').length - 1]
+  return $(img)
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('div')
+}
+
+function firstListItem() {
+  return $('ul li:first-child')
+}
